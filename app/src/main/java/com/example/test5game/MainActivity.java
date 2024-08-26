27828e90912666b371e09fb2721e13da8b3554b7
sergent12374 <sergent12374@gmail.com>
2024-08-26 18:19:21 +0800

    Game loop

--- app/src/main/java/com/example/test5game/MainActivity.java
+++ app/src/main/java/com/example/test5game/MainActivity.java
@@ -1,24 +1,24 @@
 package com.example.test5game;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Window window = getWindow();
+
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        setContentView(new Game(this));
     }
 }
